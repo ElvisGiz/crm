f5874ba634d3ac5286d97228d651f16e09e38228
Elvis <yodamorfius@gmail.com>
2022-10-09 15:57:01 +0500

    test

--- db.js
+++ db.js
@@ -1,48 +1,52 @@
-const { Client, Pool } = require('pg')
+const { Client, Pool } = require("pg");
+const generateUUID = require("./generateUUID");
 const client = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'tasks',
-    password: 'admin347',
-    port: 5432,
-  })
-client.connect()
-function query(query, data, callback){
-    //if(client)
-    
-    let queryText = ``
-    switch(query){
-        case "create":
-            queryText = `INSERT INTO tasks (title, text, dateofcreate, priority) 
+  user: "postgres",
+  host: "localhost",
+  database: "tasks",
+  password: "admin347",
+  port: 5432,
+});
+client.connect();
+function query(query, data, callback) {
+  //if(client)
+
+  let queryText = ``;
+  let uuid = generateUUID.generateUUID();
+  switch (query) {
+    case "create":
+      queryText = `INSERT INTO tasks (title, text, dateofcreate, priority, uuid) 
             VALUES ('${data.title}', '${data.text ? data.text : ""}', 
-            '${data.dateofcreate ? data.dateofcreate : ""}', '${data.priority ? data.priority : 0}')`
-            break;
-        case "comment":
-            queryText = `UPDATE tasks SET comment = '${data.comment}' WHERE id='${data.id}';`
-            break;
-        case "track":
-            queryText = `UPDATE tasks SET spenttime = '${data.spenttime}' WHERE id='${data.id}';`
-            break;    
-        case "close":
-            queryText = `UPDATE tasks
-            SET dateofclose = '${data.dateofclose}' WHERE id='${data.id}';`
-            break;    
-        case "select":
-            queryText = `SELECT * FROM tasks WHERE dateofclose is NULL`
-            break;      
-    }    
-    
-    client.query(queryText, (err, res) => {
-            if(err){
-                console.log(err)
-            }else{
-                if(callback){
-                    callback(res.rows)
-                }
-            }
-            
-        })
-        
-    
+            '${data.dateofcreate ? data.dateofcreate : ""}', '${
+        data.priority ? data.priority : 0
+      }', '${uuid}')`;
+      break;
+    case "comment":
+      queryText = `INSERT INTO comments (uuid, comment) VALUES ('${data.uuid}', '${data.comment}')`;
+      break;
+    case "track":
+      queryText = `UPDATE tasks SET spenttime = '${data.spenttime}' WHERE id='${data.id}';`;
+      break;
+    case "close":
+      queryText = `UPDATE tasks
+            SET dateofclose = '${data.dateofclose}' WHERE id='${data.id}';`;
+      break;
+    case "select":
+      queryText = `SELECT * FROM tasks WHERE dateofclose is NULL`;
+      break;
+    case "selectComments":
+      queryText = `SELECT * FROM comments`;
+      break;
+  }
+
+  client.query(queryText, (err, res) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (callback) {
+        callback(res.rows);
+      }
+    }
+  });
 }
-module.exports.query = query
+module.exports.query = query;
--- index.js
+++ index.js
@@ -1,45 +1,53 @@
-const fs = require("fs")
-const db = require("./db")
-const fastify = require('fastify')()
-fastify.register(require('@fastify/cors'), { 
-    // put your options here
-  })
-  
-  fastify.get('/', function (request, reply) {
-    let data = fs.readFileSync("./public/index.html", "utf-8")
-    reply.type('text/html')
-    reply.send(data)
-  })
+const fs = require("fs");
+const db = require("./db");
+const fastify = require("fastify")();
+fastify.register(require("@fastify/cors"), {
+  // put your options here
+});
 
-  fastify.post("/", function (request, reply) {
-    reply.type("aplication/json")
-    reply.send(request.body)
-  })
+fastify.get("/", function (request, reply) {
+  let data = fs.readFileSync("./public/index.html", "utf-8");
+  reply.type("text/html");
+  reply.send(data);
+});
 
-  fastify.post("/db", function (request, reply) {
-    reply.type("aplication/json")
-    try{
-      db.query(request.headers.query, request.body)
-    }catch(err){
-      reply.send(err)
+fastify.post("/", function (request, reply) {
+  reply.type("aplication/json");
+  reply.send(request.body);
+});
+
+fastify
+  .post("/db", function (request, reply) {
+    reply.type("aplication/json");
+    try {
+      db.query(request.headers.query, request.body);
+    } catch (err) {
+      reply.send(err);
     }
-    console.log("ПОСТ ДБ ЗАПРОС")
-    
+    console.log("ПОСТ ДБ ЗАПРОС");
   })
   .get("/db", function (request, reply) {
-    reply.type("aplication/json")
-    db.query("select", "",(res)=>{
-      reply.send( res)
-    })
-    
-      console.log("ГЕТ ДБ ЗАПРОС")
+    reply.type("aplication/json");
+    db.query("select", "", (res) => {
+      reply.send(res);
+    });
+
+    console.log("ГЕТ ДБ ЗАПРОС");
   })
-  // Run the server!
-  fastify.listen({ port: 80 }, function (err, address) {
-    console.log(address)
-    if (err) {
-      fastify.log.error(err)
-      process.exit(1)
-    }
-    // Server is now listening on ${address}
-  })
+  .get("/db/comments", function (request, reply) {
+    reply.type("aplication/json");
+    db.query("selectComments", "", (res) => {
+      reply.send(res);
+    });
+
+    console.log("ГЕТ ДБ ЗАПРОС");
+  });
+// Run the server!
+fastify.listen({ port: 80 }, function (err, address) {
+  console.log(address);
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  // Server is now listening on ${address}
+});
